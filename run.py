d28aae17ef98e3bd8413da5f8270e129d6f2a01e
Olivertrain1221 <olivetrain1996@gmail.com>
2022-01-17 16:58:41 +0000

    Add test commit message

---
+++ run.py
@@ -0,0 +1,18 @@
+import os
+# Importing Flask class
+from flask import Flask
+
+# Creating a instance of this, storing it in a variable 'app'.
+# The first argument of the Flask class, is the "name" of the application's module - our package.
+app = Flask(__name__)
+
+# Decorator used to wrap functions 
+@app.route("/")
+def index():
+    return "Hello, world"
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
