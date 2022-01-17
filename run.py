4e5cb37183a800971c53c8a4749b76dc21f9758e
Olivertrain1221 <olivetrain1996@gmail.com>
2022-01-17 18:03:43 +0000

    Add first base template and combined visible code

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import os
 # Importing Flask class
-from flask import Flask
+from flask import Flask, render_template
 
 # Creating a instance of this, storing it in a variable 'app'.
 # The first argument of the Flask class, is the "name" of the application's module - our package.
@@ -9,7 +9,23 @@ app = Flask(__name__)
 # Decorator used to wrap functions 
 @app.route("/")
 def index():
-    return "Hello, world"
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    return render_template(careers.html)
+
 
 # making environment
 if __name__ == "__main__":
@@ -17,3 +33,4 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+        ##never have debug = true in pose depoloyment
