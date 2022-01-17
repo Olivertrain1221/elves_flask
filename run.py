9628560ecbbbb12706b819bc91953aaabf0a0e36
Olivertrain1221 <olivertrain1996@gmail.com>
2022-01-17 19:46:54 +0000

    Add changes to run.py

--- run.py
+++ run.py
@@ -1,12 +1,10 @@
 import os
-# Importing Flask class
 from flask import Flask, render_template
 
-# Creating a instance of this, storing it in a variable 'app'.
-# The first argument of the Flask class, is the "name" of the application's module - our package.
+
 app = Flask(__name__)
 
-# Decorator used to wrap functions 
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -24,13 +22,11 @@ def contact():
 
 @app.route("/careers")
 def careers():
-    return render_template(careers.html)
+    return render_template("careers.html")
 
 
-# making environment
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
-        ##never have debug = true in pose depoloyment
